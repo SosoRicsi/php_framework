fbaacf21e9950ed5990b2534d1df232ff93acc9e
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-05-29 20:32:11 +0200

    Run pint in request class

--- src/Radiant/http/Request/Request.php
+++ src/Radiant/http/Request/Request.php
@@ -4,140 +4,148 @@ declare(strict_types=1);
 
 namespace Radiant\Http\Request;
 
+use Exception;
 use Radiant\Collection\SmartCollection;
 
-class Request
+final class Request
 {
-	public string $method;
-	public string $uri;
-	public array $headers;
-	public mixed $body;
-	public array $validator_errors = [];
-	public array $router_params = [];
+    public string $method;
 
-	public function __construct()
-	{
-		$this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
-		$this->uri = $_SERVER['REQUEST_URI'] ?? '/';
+    public string $uri;
 
-		$headers = function_exists('getallheaders') ? getallheaders() : $this->getAllHeadersFallback();
-		$this->headers = array_change_key_case($headers, CASE_LOWER);
+    public array $headers;
 
-		$this->body = file_get_contents('php://input');
-	}
+    public mixed $body;
 
-	public function getMethod(): string
-	{
-		return $this->method;
-	}
+    public array $validator_errors = [];
 
-	public function getUri(): string
-	{
-		return $this->uri;
-	}
+    public array $router_params = [];
 
-	public function getResolvedUri(): array
-	{
-		$path = parse_url($this->uri, PHP_URL_PATH);
-		return explode('/', trim($path, '/'));
-	}
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+        $this->uri = $_SERVER['REQUEST_URI'] ?? '/';
 
-	public function getHeaders(): array
-	{
-		return $this->headers;
-	}
+        $headers = function_exists('getallheaders') ? getallheaders() : $this->getAllHeadersFallback();
+        $this->headers = array_change_key_case($headers, CASE_LOWER);
 
-	public function getHeader(string $key, mixed $default = null): mixed
-	{
-		$key = strtolower($key);
-		return $this->headers[$key] ?? $default;
-	}
+        $this->body = file_get_contents('php://input');
+    }
 
-	public function setBody(string|array $body): void
-	{
-		$this->body = is_array($body) ? json_encode($body) : $body;
-	}
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
 
-	public function mergeBody(array $newData): void
-	{
-		$existingData = is_string($this->body) ? json_decode($this->body, true) : $this->body;
+    public function getUri(): string
+    {
+        return $this->uri;
+    }
 
-		if (json_last_error() !== JSON_ERROR_NONE) {
-			throw new \Exception("Invalid JSON format in body during merge.");
-		}
+    public function getResolvedUri(): array
+    {
+        $path = parse_url($this->uri, PHP_URL_PATH);
 
-		$merged = array_merge($existingData ?? [], $newData);
-		$this->body = json_encode($merged);
-	}
+        return explode('/', trim($path, '/'));
+    }
 
-	public function getBody(?string $key = null): mixed
-	{
-		$data = is_string($this->body) ? json_decode($this->body, true) : $this->body;
+    public function getHeaders(): array
+    {
+        return $this->headers;
+    }
 
-		if (json_last_error() !== JSON_ERROR_NONE) {
-			throw new \Exception("Invalid JSON format in request body!");
-		}
+    public function getHeader(string $key, mixed $default = null): mixed
+    {
+        $key = mb_strtolower($key);
 
-		return $key !== null ? ($data[$key] ?? null) : new SmartCollection($data);
-	}
+        return $this->headers[$key] ?? $default;
+    }
 
-	public function getJsonBody(?string $key = null): mixed
-	{
-		$data = is_array($this->body) ? $this->body : json_decode($this->body, true);
+    public function setBody(string|array $body): void
+    {
+        $this->body = is_array($body) ? json_encode($body) : $body;
+    }
 
-		if (json_last_error() !== JSON_ERROR_NONE) {
-			throw new \Exception("Invalid JSON format!");
-		}
+    public function mergeBody(array $newData): void
+    {
+        $existingData = is_string($this->body) ? json_decode($this->body, true) : $this->body;
 
-		return $key !== null ? ($data[$key] ?? null) : $data;
-	}
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Invalid JSON format in body during merge.');
+        }
 
-	public function get(string $key = null, mixed $default = null): mixed
-	{
-		return $key !== null ? ($_GET[$key] ?? $default) : $_GET;
-	}
+        $merged = array_merge($existingData ?? [], $newData);
+        $this->body = json_encode($merged);
+    }
 
-	public function isContentType(string $type): bool
-	{
-		return $this->getHeader('Content-Type') === $type;
-	}
+    public function getBody(?string $key = null): mixed
+    {
+        $data = is_string($this->body) ? json_decode($this->body, true) : $this->body;
 
-	public function isJSON(): bool
-	{
-		return $this->isContentType('application/json');
-	}
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Invalid JSON format in request body!');
+        }
 
-	public function isFormData(): bool
-	{
-		return $this->isContentType('multipart/form-data');
-	}
+        return $key !== null ? ($data[$key] ?? null) : new SmartCollection($data);
+    }
 
-	public function setRouterParams(array $params): void
-	{
-		$this->router_params = $params;
-	}
+    public function getJsonBody(?string $key = null): mixed
+    {
+        $data = is_array($this->body) ? $this->body : json_decode($this->body, true);
 
-	public function rParam(string $param, mixed $default = null): mixed
-	{
-		return $this->router_params[$param] ?? $default;
-	}
+        if (json_last_error() !== JSON_ERROR_NONE) {
+            throw new Exception('Invalid JSON format!');
+        }
 
-	public function rParams(): array
-	{
-		return $this->router_params;
-	}
+        return $key !== null ? ($data[$key] ?? null) : $data;
+    }
 
-	private function getAllHeadersFallback(): array
-	{
-		$headers = [];
+    public function get(?string $key = null, mixed $default = null): mixed
+    {
+        return $key !== null ? ($_GET[$key] ?? $default) : $_GET;
+    }
 
-		foreach ($_SERVER as $name => $value) {
-			if (str_starts_with($name, 'HTTP_')) {
-				$header = str_replace('_', '-', substr($name, 5));
-				$headers[$header] = $value;
-			}
-		}
+    public function isContentType(string $type): bool
+    {
+        return $this->getHeader('Content-Type') === $type;
+    }
 
-		return $headers;
-	}
+    public function isJSON(): bool
+    {
+        return $this->isContentType('application/json');
+    }
+
+    public function isFormData(): bool
+    {
+        return $this->isContentType('multipart/form-data');
+    }
+
+    public function setRouterParams(array $params): void
+    {
+        $this->router_params = $params;
+    }
+
+    public function rParam(string $param, mixed $default = null): mixed
+    {
+        return $this->router_params[$param] ?? $default;
+    }
+
+    public function rParams(): array
+    {
+        return $this->router_params;
+    }
+
+    private function getAllHeadersFallback(): array
+    {
+        $headers = [];
+
+        foreach ($_SERVER as $name => $value) {
+            if (str_starts_with($name, 'HTTP_')) {
+                $header = str_replace('_', '-', mb_substr($name, 5));
+                $headers[$header] = $value;
+            }
+        }
+
+        return $headers;
+    }
 }
