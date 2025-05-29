589b84e621661e6d1c791415434649a2b29823c1
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-05-29 20:35:37 +0200

    Run pint in test files

--- tests/Mocks/MockPhpStream.php
+++ tests/Mocks/MockPhpStream.php
@@ -1,29 +1,35 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Tests\Mocks;
 
-class MockPhpStream
+final class MockPhpStream
 {
-    private $index = 0;
-    private string $data;
     public $context;
+
+    private $index = 0;
+
+    private string $data;
 
     public function stream_open($path, $mode, $options, &$opened_path): bool
     {
         $this->data = '{"name": "Ricsi", "age": 30}';
+
         return true;
     }
 
     public function stream_read($count): string
     {
-        $chunk = substr($this->data, $this->index, $count);
-        $this->index += strlen($chunk);
+        $chunk = mb_substr($this->data, $this->index, $count);
+        $this->index += mb_strlen($chunk);
+
         return $chunk;
     }
 
     public function stream_eof(): bool
     {
-        return $this->index >= strlen($this->data);
+        return $this->index >= mb_strlen($this->data);
     }
 
     public function stream_stat(): array
