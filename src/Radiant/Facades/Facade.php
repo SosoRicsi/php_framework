468a8c81d922720c7ea8b7eb9462d5e7c477ce91
Jarkó Richárd <jarkoricsi16@gmail.com>
2025-05-30 17:38:29 +0200

    Add facade

---
+++ src/Radiant/Facades/Facade.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Radiant\Facades;
+
+abstract class Facade
+{
+	protected static array $instances = [];
+
+	abstract protected static function getFacadeAccessor(): string;
+
+	protected static function resolveInstance(): object
+	{
+		$key = static::getFacadeAccessor();
+
+		if (!isset(self::$instances[$key])) {
+			self::$instances[$key] = static::registerAccessor();
+		}
+
+		return self::$instances[$key];
+	}
+
+	abstract protected static function registerAccessor(): object;
+
+	public static function __callStatic($method, $args)
+	{
+		$instance = static::resolveInstance();
+
+		if (!method_exists($instance, $method)) {
+			throw new \BadMethodCallException("Method {$method} does not exist on class " . get_class($instance));
+		}
+
+		return $instance->$method(...$args);
+	}
+}
